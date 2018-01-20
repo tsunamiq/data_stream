6021772c097c8734738e0d7925844befcb5e1ff0
Jonathan Thinh <jonathanthinh@Jonathans-MBP.attlocal.net>
2018-01-19 20:19:33 -0800

    stream data

--- data.js
+++ data.js
@@ -1,46 +1,80 @@
 const Gdax = require('gdax');
 const publicClient = new Gdax.PublicClient();
 
-
 const apiURI = 'https://api.gdax.com';
 const sandboxURI = 'https://api-public.sandbox.gdax.com';
 
-
 const websocket = new Gdax.WebsocketClient(['BTC-USD', 'ETH-USD']);
-
 
 var fs = require('fs');
 
 
-setInterval(function(){ getTicker() }, 1000);
 
+class streamData {
+	constructor(){
+		this.tickerData = {};
+		this.timeCompare = "";
+	}
 
+	storeData(){
+		var dataArray = [];
+		publicClient.getProductTicker('BTC-USD',(err, response, data) =>{
+			if(err){
+				console.log("error with getTicker function");
+			}else{
+				dataArray = [data.trade_id,data.price, data.size, data.bid, data.ask, data.volume, data.time];
+				console.log(dataArray);
 
+				// fs.appendFile("data.txt",dataArray , function(err) {
+				//     if(err) {
+				//         return console.log(err);
+				//  }
 
+				 fs.open('data.txt', 'a', 666, function( e, id ) {
+				   fs.write( id, dataArray + "\n", null, 'utf8', function(){
+				 
+				     console.log('file is updated');
+				    });
+				
+				  });
+			}
+		});
+	}
 
+	priceFeed(){
+		let _this = this; 
 
-let  getTicker = () => {
-	var dataArray = [];
-	publicClient.getProductTicker('BTC-USD',(err, response, data) =>{
-		if(err){
-			console.log("error with getTicker function");
-		}else{
-			dataArray = [data.trade_id,data.price, data.size, data.bid, data.ask, data.volume, data.time];
-			console.log(dataArray);
+		publicClient.getProductTicker('BTC-USD',(err, response, data) =>{
+			if(err){
+				console.log("==================================")
+				console.log("error with getTicker function");
+				console.log(err)
+				console.log("==================================")
+			}else{		
+				_this.tickerData = {
+					"timestamp": data.time,
+					"current_price": data.price
+				}
+			}
+		});
+	}
 
-			// fs.appendFile("data.txt",dataArray , function(err) {
-			//     if(err) {
-			//         return console.log(err);
-			//  }
+	getCurrentPrice(){
+		return this.tickerData;
+	}
 
-			 fs.open('data.txt', 'a', 666, function( e, id ) {
-			   fs.write( id, dataArray + "\n", null, 'utf8', function(){
-			 
-			     console.log('file is updated');
-			    });
-			
-			  });
-		}
-	});
+	init(){
+		let _this = this;
+		setInterval(function(){
+			_this.priceFeed();
+		},1000)
+	}
 }
 
+
+var stream = new streamData();
+setInterval(function(){
+	stream.storeData();
+},1000)
+
+module.exports = streamData;
